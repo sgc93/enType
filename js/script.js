b0923b22156b84235b9cbe6b969ef47f37587194
sgc93 <smachewgedefaw@gmail.com>
2023-12-15 09:47:49 -0900

    create temp data and change it to array

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const words =
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit Harum veniam ipsam molestias aut at dolorem ratione non Obcaecati porro cumque error enim officia dolores soluta incidunt aliquam vel necessitatibus repellat".split(
+		" "
+	);
