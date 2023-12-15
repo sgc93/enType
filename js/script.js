4f80457af0324d57d78404c0ce00b029eeab4573
sgc93 <smachewgedefaw@gmail.com>
2023-12-15 10:07:19 -0900

    generate randome formatted word and display it to game rea

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ const words =
 	"Lorem ipsum dolor sit amet consectetur adipisicing elit Harum veniam ipsam molestias aut at dolorem ratione non Obcaecati porro cumque error enim officia dolores soluta incidunt aliquam vel necessitatibus repellat".split(
 		" "
 	);
+const wordsCount = words.length;
+
+// get random word from the words' array
+const getRandomWord = () => {
+	const randomIndex = Math.ceil(Math.random() * wordsCount);
+	console.log(words[randomIndex] + " " + randomIndex);
+	return words[randomIndex];
+};
+
+// format a random word
+
+const formatWord = (word) => `<div class="word">${word}</div>`;
+
+// initiate game
+
+const newGame = () => {
+	document.getElementById("words").innerHTML = ""; // clearing
+	for (var i = 0; i < 200; i++) {
+		document.getElementById("words").innerHTML += formatWord(getRandomWord());
+	}
+};
+
+newGame();
