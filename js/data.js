6f1557974b266ad6e003510c31cf8e88f82d3610
sgc93 <smachewgedefaw@gmail.com>
2023-12-27 04:06:44 -0900

    add words under different category

---
+++ js/data.js
@@ -0,0 +1,48 @@
+const easyEng =
+	"do you want to type faster the average typing speed is around wpm but more productive typists can average around wpm there is no special secret to fast typing and anyone can become a fast typist with patience and dedication while it is not complicated it does require correct posture and finger positioning this article gives you some tips and tricks to typing faster things you should know sit with your feet flat on the floor and your monitor level with your eyes do not slouch arch your fingers over the keys know that smaller movements are faster than bigger movements there are plenty of free resources like typing games and tests online for you to practice with practice at least minutes a day get to know the keyboard most keyboards use the qwerty layout but azerty and qwertz are also common look at your keyboard to see what layout you have and where buttons like shift and enter are practice memorizing the positions of the letter keys and common punctuation marks you need to know their location without looking down if you want to become a fast typist however some of this knowledge comes from time as you continuously type without looking at the keyboard ".split(
+		" "
+	);
+
+const hardEng =
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry! It has survived not only five centuries but also the leap into 17th place. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there is not anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words like 5, 6, 7, 8, 9, 10, which do not make sense.".split(
+		" "
+	);
+
+const javaCode =
+	`import java.util.Scanner; public class Calculator { public static void main(String[] args) { Scanner scanner = new Scanner(System.in); // Accepting the first number System.out.print("Enter the first number: "); double number1 = scanner.nextDouble( // Accepting the second number System.out.print("Enter the second number: "); double number2 = scanner.nextDouble( // Calculating and printing the sum double sum = number1 + number2; System.out.println("Sum: " + sum // Calculating and printing the difference double difference = number1 - number2; System.out.println("Difference: " + difference // Checking if division by zero is possible if (number2 != 0) { // Calculating and printing the division double division = number1 / number2; System.out.println("Division: " + division); } else { System.out.println("Cannot divide by zero."); // Calculating and printing the product double product = number1 * number2; System.out.println("Product: " + product // Closing the scanner scanner.close() }`.split(
+		" "
+	);
+
+const dartCode = `import 'dart:io';
+
+void main() {
+  // Accepting the first number
+  stdout.write('Enter the first number: ');
+  double number1 = double.parse(stdin.readLineSync()!);
+
+  // Accepting the second number
+  stdout.write('Enter the second number: ');
+  double number2 = double.parse(stdin.readLineSync()!);
+
+  // Calculating and printing the sum
+  double sum = number1 + number2;
+  print('Sum: $sum');
+
+  // Calculating and printing the difference
+  double difference = number1 - number2;
+  print('Difference: $difference');
+
+  // Checking if division by zero is possible
+  if (number2 != 0) {
+    // Calculating and printing the division
+    double division = number1 / number2;
+    print('Division: $division');
+  } else {
+    print('Cannot divide by zero.');
+  }
+
+  // Calculating and printing the product
+  double product = number1 * number2;
+  print('Product: $product');
+}
+`.split(" ");
