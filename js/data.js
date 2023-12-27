e4380b49b85afa67f42872c20c8d8f1353995e06
sgc93 <smachewgedefaw@gmail.com>
2023-12-27 11:58:49 -0900

    update languages' keywords

--- js/data.js
+++ js/data.js
@@ -12,39 +12,36 @@ const hardEng =
 	'@Tiger#42Roar! The encrypted message reads: 5$ecureP@ssword! Unlock the vault with code 12345. Beware of the darkness that lurks in the shadows. A=πr²: calculate the area of the circle. The quick brown fox jumps over the 1azy dog! "To be or not to be," a famous Shakespearean dilemma. 3.14159265359, the never-ending dance of pi. The cyborg revolution begins on 01/20/3035. @QuantumPhysics: E=mc², unraveling the mysteries of the cosmos. Can you solve the riddle: What has keys but can\'t open locks?'.split(
 		" "
 	);
+const jsCode =
+	"var let const function return if else for in while do switch case break continue try catch throw finally typeof null undefined true false NaN Infinity async await new this super class extends prototype static".split(
+		" "
+	);
+
+const pythonCode =
+	"and as assert break class continue def del elif else except False finally for from global if import in is lambda None nonlocal not or pass raise return True try while with yield".split(
+		" "
+	);
 
 const javaCode =
 	`abstract assert boolean break byte case catch char class const continue default do // double else enum extends final finally float for if goto implements import instanceof int interface long native new package private protected public return short static strictfp super switch synchronized this throw . /**/ throws transient try void volatile while String arrays ArrayList LinkedList HashMap HashSet TreeMap TreeSet Calendar Date NumberFormat Random Math StringBuffer StringBuilder FileReader {} FileWriter PrintWriter Scanner BufferedReader BufferedWriter Socket ServerSocket SocketException IOException URL URI URISyntaxException MalformedURLException URLConnection HttpURLConnection InputStream OutputStream FileInputStream [] FileOutputStream ObjectInputStream ObjectOutputStream DataInputStream DataOutputStream Reader Writer PrintStream System out err in getProperty ;
 `.split(" ");
 
-const dartCode = `import 'dart:io'; void main() {
-  // Accepting the first number
-  stdout.write('Enter the first number: ');
-  double number1 = double.parse(stdin.readLineSync()!);
+const dartCode =
+	"class enum abstract extends implements final super this static const var void return new if else for in while do switch case break continue try catch throw assert null true false dynamic void Function Object String int double num bool List Map Set Runes Symbol".split(
+		" "
+	);
 
-  // Accepting the second number
-  stdout.write('Enter the second number: ');
-  double number2 = double.parse(stdin.readLineSync()!);
+const cSharp =
+	"abstract as base bool break byte case catch char checked class const continue decimal default delegate do double else enum event explicit extern false finally fixed float for foreach goto if implicit in int interface internal is lock long namespace new null object operator out override params private protected public readonly ref return sbyte sealed short sizeof stackalloc static string struct switch this throw true try typeof uint ulong unchecked unsafe ushort using virtual void volatile while".split(
+		" "
+	);
 
-  // Calculating and printing the sum
-  double sum = number1 + number2;
-  print('Sum: $sum');
+const cPlusCode =
+	"asm auto bool break case catch char class const const_cast continue default delete do double dynamic_cast else enum explicit export extern false float for friend goto if inline int long mutable namespace new operator private protected public register reinterpret_cast return short signed sizeof static static_cast struct switch template this throw true try typedef typeid typename union unsigned using virtual void volatile wchar_t while".split(
+		" "
+	);
 
-  // Calculating and printing the difference
-  double difference = number1 - number2;
-  print('Difference: $difference');
-
-  // Checking if division by zero is possible
-  if (number2 != 0) {
-    // Calculating and printing the division
-    double division = number1 / number2;
-    print('Division: $division');
-  } else {
-    print('Cannot divide by zero.');
-  }
-
-  // Calculating and printing the product
-  double product = number1 * number2;
-  print('Product: $product');
-}
-`.split(" ");
+const cCode =
+	"auto double int struct break else long switch case enum register typedef char extern return union const float short unsigned continue for signed void default goto sizeof volatile do if static while".split(
+		" "
+	);
