cc96096e194eb5ae4ec99ae0dafa533a18b4d10f
sgc93 <smachewgedefaw@gmail.com>
2023-12-27 08:20:24 -0900

    use sample words from data.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-const words =
-	"Lorem ipsum dolor sit amet consectetur adipisicing elit Harum veniam ipsam molestias aut at dolorem ratione non Obcaecati porro cumque error enim officia dolores soluta incidunt aliquam vel necessitatibus repellat".split(
-		" "
-	);
+const words = easyEng;
 const wordsCount = words.length;
 const gameTime = 30 * 1000;
 window.timer = null;
